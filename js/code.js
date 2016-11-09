d597ed20235ce806fe0e9f38705e89df5ba11cde
Shuhan Jiang <mac@default.resnet1.tc.columbia.edu>
2016-11-09 15:02:01 +0800

    change
    update

--- js/code.js
+++ js/code.js
@@ -18,3 +18,17 @@ for(var index=0;index<tabs.length;index++){
 }
 
 document.getElementById('submit').addEventListener('click',function(){alert(" Submitted! Good job!")})
+// document.getElementById('submit2').addEventListener('click',function(){alert(" That's it! Keep working!")})
+
+var x1 = document.getElementById("c1").checked;
+var x2 = document.getElementById("c2").checked;
+var x3 = document.getElementById("c3").checked;
+var x4 = document.getElementById("c4").checked;
+var x5 = document.getElementById("c5").checked;
+
+
+document.getElementById('submit2').addEventListener('click',function(){
+	if(x1)
+	{alert(" That's it! Keep working!")}
+	else
+	{alert(" There's something else! Try again!")}})
